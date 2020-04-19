2f19c6cd56c13c229ebb53427825ce5a3ae1398c
Emanuel Lorenzo <aemalorenzo@gmail.com>
2020-04-20 02:35:01 +0900

    Basic setup

---
+++ app/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+export const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+app.use(morgan('dev'));
+
+app.use('/', (_, res) => {
+  res.status(200).send('YO!');
+});
+
+export const start = async () => {
+  try {
+    mongoose.connect(process.env.DB_URL || '', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`REST API on http://localhost:${process.env.PORT}/api`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
